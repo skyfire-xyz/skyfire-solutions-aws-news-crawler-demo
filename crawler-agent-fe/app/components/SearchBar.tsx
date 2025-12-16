fc8ab376b107db5c6b0a75eada99f860b1432c8e
Chloe Ta <chloe@skyfire.xyz>
2025-12-16 11:00:47 -0500

    created crawling context to be used by navTabs to restrict tab switch during crawling

--- crawler-agent-fe/app/components/SearchBar.tsx
+++ crawler-agent-fe/app/components/SearchBar.tsx
@@ -15,6 +15,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { useCrawling } from "../contexts/CrawlingContext"
 
 import { Alert, AlertType } from "../types"
 
@@ -109,7 +110,7 @@ const SearchBar: React.FC<SearchBarProps> = ({
   pageRoute,
 }) => {
   const [kyaToken, setKyaToken] = useState<string>(skyfireKyaToken || "")
-  const [isLoading, setIsLoading] = useState(false)
+  const { isCrawling: isLoading, setIsCrawling: setIsLoading } = useCrawling()
   const [botTypes, setBotTypes] = useState<BotTypes[]>([])
 
   useEffect(() => {
